c047e24e68f08781eb5eba72f1a4f82942983517
Vansteenk <62255688+vansteenk@users.noreply.github.com>
2020-05-28 09:55:18 +0200

    Create Etude Fiabilité.mk

---
+++ Etude Fiabilité.mk
@@ -0,0 +1,24 @@
+
+# Etude Fiabilité
+
+## HSRP
+
+#### Coupure de DS2
+
+Ping fonctionnel 
+
+#### Coupure de DS1
+
+`ping www.google.com` continu de centos-1. Petite latence de quelques secondes avant reprise.
+
+## Spanning Tree et Etherchannel
+
+#### Coupure d'un brin de po2 puis des 2 brins
+
+Ping fonctionnel
+
+#### Remise de po2
+
+reprise du traffic sur po2 [Capture à mettre]
+
+
